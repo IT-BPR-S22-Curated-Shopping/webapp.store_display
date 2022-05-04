c04cd14bfc6c5ec8fd138ff12f96dea4bb4a140c
anderssonderby <50653739+anderssonderby@users.noreply.github.com>
2022-05-04 16:22:33 +0200

    Change web app to use WebSockets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import * as React from "react";
-import {useEffect, useState} from "react";
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
-import ShowcaseComponent from "./ShowcaseComponent";
-const mqtt = require('mqtt/dist/mqtt');
+import {useEffect} from "react";
 
 function App() {
-    const [beacon, setBeacon] = useState({})
-    const [beaconTime, setBeaconTime] = useState(new Date())
-    const [tick, setTick] = useState(1)
+    // const [tick, setTick] = useState(1)
 
     // useEffect(() => {
     //     let secondsBetween = (new Date().getTime() - beaconTime.getTime()) / 1000;
@@ -24,60 +18,17 @@ function App() {
     //     setInterval(() => {
     //         setTick(Math.random);
     //     }, 1000)
-    //
-    //     const host = 'ws://10.0.4.10:8080'
-    //     const topic = ['0462/rpi3/beacon']
-    //
-    //     const mqttOptions = {
-    //         clientId: 'SD' + Math.random().toString(16),
-    //         protocolId: 'MQTT',
-    //         protocolVersion: 4,
-    //         username: 'anchor',
-    //         password: 'BLEtracker',
-    //         reconnect: true,
-    //         reconnectPeriod: 1000,
-    //         keepAlive: 30,
-    //         clean: true,
-    //         rejectUnauthorized: false
-    //     }
-    //     const mqttClient = mqtt.connect(host, mqttOptions)
-    //
-    //     mqttClient.on('connect', () => {
-    //         console.log('Mqtt connected.')
-    //         mqttClient.subscribe(topic, () => {
-    //             console.log(`Subscribed to topic '${topic}'`)
-    //         })
-    //     })
-    //
-    //     mqttClient.on("error", (error) => {
-    //         console.log("Can't connect" + error);
-    //         process.exit(1)
-    //     });
-    //
-    //     mqttClient.on('message', (topic, payload) => {
-    //         const msg = JSON.parse(payload.toString())
-    //         console.log(msg)
-    //         setBeaconTime(new Date())
-    //         setBeacon(msg);
-    //     })
-    //
-    //     mqttClient.on('close', () => {
-    //         console.log('MQTT disconnected')
-    //     })
     // }, [])
 
     useEffect(() => {
-        let socket = SockJS("http://localhost:9000/curated-shopping");
-        let stompClient = Stomp.over(socket);
+        let webSocket = new WebSocket('ws://localhost:9000/presentation');
+        webSocket.onmessage = function (payload) {
+            console.log(payload.data)
+        }
 
-        stompClient.connect({}, (sock) => {
-            console.log("Connected to STOMP socket: " + sock)
-
-            stompClient.subscribe('presentation/uuid', (uuid) => {
-                console.log("UUID: " + uuid + " received")
-            })
-            console.log("Subscribed to topic")
-        })
+        webSocket.addEventListener('open', function (event) {
+            webSocket.send("Callback from presentation device");
+        });
 
     }, []);
 
